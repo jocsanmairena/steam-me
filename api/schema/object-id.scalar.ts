880eb6026265bd3cb279968407613bf5b62cba7a
jocsanmairena <jocsan.mairena@gmail.com>
2021-02-16 12:49:48 -0500

    17 ObjectIdScalar corrections

--- api/schema/object-id.scalar.ts
+++ api/schema/object-id.scalar.ts
@@ -4,11 +4,12 @@ In summary, ObjectID("adaj130jfsdm10") can be converted to text string: adaj130j
 Graphql comes bundle with scalar types for string, int, Id, etc. But none with mongodb ObjectId format.
 GraphQlScalarType: This graphql scalar type can define custom scalar types for us to work with.
 */
-import { GraphQLScalarType, Kind, parseValue } from 'graphql'
+import { GraphQLScalarType, Kind } from 'graphql'
 import { ObjectId } from 'mongodb'
 
 
 export const ObjectIdScalar = new GraphQLScalarType({
+
   name: "ObjectId",
   description: "Mongo Id scalar type",
 
@@ -27,25 +28,6 @@ export const ObjectIdScalar = new GraphQLScalarType({
       return new ObjectId(ast.value) //value from client query
     }
     return null
-
   }
 
-
-
 })
-// export const ObjectIdScalar = new GraphQLScalarType({
-//   name: "ObjectId",
-//   description: "Mongo object id scalar type",
-//   parseValue(value: string) {
-//     return new ObjectId(value) // value from the client input variables
-//   },
-//   serialize(value: ObjectId) {
-//     return value.toHexString() // value sent to the client
-//   },
-//   parseLiteral(ast) {
-//     if (ast.kind === Kind.STRING) {
-//       return new ObjectId(ast.value) // value from the client query
-//     }
-//     return null
-//   },
-// })
